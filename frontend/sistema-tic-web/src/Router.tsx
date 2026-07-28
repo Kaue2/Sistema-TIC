d88d574acd9301ca60e437126338d3f24479beaf
Phelipe Pereira <phelipe.psouza@senacsp.edu.br>
2026-07-28 12:30:55 -0300

    feat: add as rotas da aplicacao até o momento

--- frontend/sistema-tic-web/src/Router.tsx
+++ frontend/sistema-tic-web/src/Router.tsx
@@ -1,9 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Login } from "./pages/Login";
+import { ProfilePage } from "./pages/ProfilePage";
+import { WelcomeConfirmation } from "./pages/WelcomeConfirmation";
+import { AccessUpdate } from "./pages/AccessUpdate";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />, // Rota pública
+  },
+  {
+    path: "/welcome",
+    element: <WelcomeConfirmation />, // Rota pública
+  },
+  {
+    path: "/access-update",
+    element: <AccessUpdate />, // Rota pública
+  },
+  {
+    path: "/profile/:id",
+    element: <ProfilePage />,
   },
 ]);
